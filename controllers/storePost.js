71cefd7aa53a8fe721bf4c426310ca9a6327235b
rpparede <rpparede@uwaterloo.ca>
2021-01-06 00:44:21 -0800

    Adding more logs

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -6,6 +6,10 @@ module.exports = (req, res) => {
   const { image } = req.files
   const uploadPath = path.resolve(__dirname, '..', "public/posts", image.name)
   image.mv(uploadPath, (error) => {
+    if (error) {
+      console.log("**ERROR  MOVING FILE TO PATH**")
+      console.log(error)
+    }
     cloudinary.v2.uploader.upload(uploadPath, (error, result) => {
 
       if (error) {
